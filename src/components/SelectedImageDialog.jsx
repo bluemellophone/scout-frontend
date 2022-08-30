ef36c0ffe35e0819d03a06367274a7fcf10086b1
Ben Scheiner <brmscheiner@gmail.com>
2022-08-30 13:44:20 -0700

    Migrate selected image dialog and delete task dialog

--- src/components/SelectedImageDialog.jsx
+++ src/components/SelectedImageDialog.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useMemo } from 'react';
 import { get, differenceBy } from 'lodash-es';
 
-import DialogContent from '@material-ui/core/DialogContent';
 import Autocomplete from '@material-ui/lab/Autocomplete';
 import TextField from '@material-ui/core/TextField';
 
@@ -9,7 +8,7 @@ import useAddKeyword from '../models/keywords/useAddKeyword';
 import useGetKeywords from '../models/keywords/useGetKeywords';
 import { formatDate } from '../utils/formatters';
 import { getKeywordColor } from '../utils/colorUtils';
-import StandardDialog from './StandardDialog';
+import StandardDialog from './StandardDialogNew';
 import Text from './Text';
 import Keywords from './Keywords';
 import Button from './ButtonNew';
@@ -61,152 +60,143 @@ export default function SelectedImageDialog({
       open={open}
       onClose={onCloseDialog}
       title="Selected image"
+      renderDialogActions={false}
     >
-      <DialogContent>
-        <img
-          src={asset?.src}
-          alt={asset?.filename}
-          style={{ width: '100%' }}
-        />
-        <div
-          style={{
-            display: 'flex',
-            justifyContent: 'space-between',
-            alignItems: 'flex-start',
-            marginBottom: 80,
-          }}
-        >
-          <div>
-            {addKeywordError && (
-              <Alert
-                style={{ marginTop: 16, marginBottom: 8 }}
-                severity="error"
-                onClose={clearAddKeywordError}
-                title="Server error"
-                description={addKeywordError}
-              />
-            )}
-            <Keywords
-              asset={asset}
-              missionGuid={missionGuid}
-              deletable
-            >
-              {addingTag ? (
-                <div
-                  style={{ display: 'flex', alignItems: 'center' }}
-                >
-                  <Autocomplete
-                    id="tag value"
-                    freeSolo
-                    blurOnSelect
-                    clearOnEscape
-                    handleHomeEndKeys
-                    selectOnFocus
-                    value={newTagSelectValue}
-                    onChange={(_, newValue) => {
-                      setNewTagSelectValue(newValue);
-                    }}
-                    inputValue={newTagInputValue}
-                    onInputChange={(_, newValue) => {
-                      setNewTagInputValue(newValue);
-                    }}
-                    disabled={addKeywordLoading}
-                    options={filteredKeywordOptions}
-                    getOptionLabel={option =>
-                      get(option, 'value', '')
-                    }
-                    renderOption={option => (
+      <img
+        src={asset?.src}
+        alt={asset?.filename}
+        style={{ width: '100%' }}
+      />
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'flex-start',
+          marginBottom: 80,
+        }}
+      >
+        <div>
+          {addKeywordError && (
+            <Alert
+              style={{ marginTop: 16, marginBottom: 8 }}
+              severity="error"
+              onClose={clearAddKeywordError}
+              title="Server error"
+              description={addKeywordError}
+            />
+          )}
+          <Keywords asset={asset} missionGuid={missionGuid} deletable>
+            {addingTag ? (
+              <div style={{ display: 'flex', alignItems: 'center' }}>
+                <Autocomplete
+                  id="tag value"
+                  freeSolo
+                  blurOnSelect
+                  clearOnEscape
+                  handleHomeEndKeys
+                  selectOnFocus
+                  value={newTagSelectValue}
+                  onChange={(_, newValue) => {
+                    setNewTagSelectValue(newValue);
+                  }}
+                  inputValue={newTagInputValue}
+                  onInputChange={(_, newValue) => {
+                    setNewTagInputValue(newValue);
+                  }}
+                  disabled={addKeywordLoading}
+                  options={filteredKeywordOptions}
+                  getOptionLabel={option => get(option, 'value', '')}
+                  renderOption={option => (
+                    <div
+                      style={{
+                        display: 'flex',
+                        alignItems: 'center',
+                      }}
+                    >
                       <div
                         style={{
-                          display: 'flex',
-                          alignItems: 'center',
+                          width: 12,
+                          height: 12,
+                          marginRight: 8,
+                          borderRadius: 100,
+                          backgroundColor: getKeywordColor(
+                            option.guid,
+                          ),
                         }}
-                      >
-                        <div
-                          style={{
-                            width: 12,
-                            height: 12,
-                            marginRight: 8,
-                            borderRadius: 100,
-                            backgroundColor: getKeywordColor(
-                              option.guid,
-                            ),
-                          }}
-                        />
-                        <span>{option.value}</span>
-                      </div>
-                    )}
-                    renderInput={params => (
-                      <TextField
-                        {...params}
-                        autoFocus
-                        style={{ width: 200, margin: '0 8px' }}
                       />
-                    )}
-                  />
-                  <Button
-                    display="tag"
-                    icon="add"
-                    loading={addKeywordLoading}
-                    onClick={async () => {
-                      const selectValue = get(
-                        newTagSelectValue,
-                        'value',
-                      );
-                      const selectKeywordId = get(
-                        newTagSelectValue,
-                        'guid',
-                      );
-                      const matchingKeywordId =
-                        newTagInputValue === selectValue
-                          ? selectKeywordId
-                          : null;
-
-                      const successful = await addKeyword({
-                        assetGuid: asset?.guid,
-                        keywordGuid: matchingKeywordId,
-                        keywordValue: newTagInputValue,
-                        missionGuid,
-                      });
-                      if (successful) {
-                        setNewTagInputValue('');
-                        setNewTagSelectValue(null);
-                      }
-                    }}
-                  >
-                    Add tag
-                  </Button>
-                </div>
-              ) : (
+                      <span>{option.value}</span>
+                    </div>
+                  )}
+                  renderInput={params => (
+                    <TextField
+                      {...params}
+                      autoFocus
+                      style={{ width: 200, margin: '0 8px' }}
+                    />
+                  )}
+                />
                 <Button
                   display="tag"
                   icon="add"
-                  onClick={() => setAddingTag(true)}
+                  loading={addKeywordLoading}
+                  onClick={async () => {
+                    const selectValue = get(
+                      newTagSelectValue,
+                      'value',
+                    );
+                    const selectKeywordId = get(
+                      newTagSelectValue,
+                      'guid',
+                    );
+                    const matchingKeywordId =
+                      newTagInputValue === selectValue
+                        ? selectKeywordId
+                        : null;
+
+                    const successful = await addKeyword({
+                      assetGuid: asset?.guid,
+                      keywordGuid: matchingKeywordId,
+                      keywordValue: newTagInputValue,
+                      missionGuid,
+                    });
+                    if (successful) {
+                      setNewTagInputValue('');
+                      setNewTagSelectValue(null);
+                    }
+                  }}
                 >
                   Add tag
                 </Button>
-              )}
-            </Keywords>
-          </div>
-          <div>
-            <Text variant="body2">{`Filename: ${
-              asset?.filename
-            }`}</Text>
-            <Text variant="body2">{`Date added: ${formatDate(
-              asset?.created,
-            )}`}</Text>
-            <Text variant="body2">{`Last updated: ${formatDate(
-              asset?.updated,
-            )}`}</Text>
-            <Text variant="body2">{`Dimensions: ${
-              asset?.dimensions?.width
-            } Χ ${asset?.dimensions?.height} pixels `}</Text>
-            <Text variant="body2">{`Size on disk: ${
-              asset?.size_bytes
-            } bytes`}</Text>
-          </div>
+              </div>
+            ) : (
+              <Button
+                display="tag"
+                icon="add"
+                onClick={() => setAddingTag(true)}
+              >
+                Add tag
+              </Button>
+            )}
+          </Keywords>
         </div>
-      </DialogContent>
+        <div>
+          <Text variant="body2">{`Filename: ${
+            asset?.filename
+          }`}</Text>
+          <Text variant="body2">{`Date added: ${formatDate(
+            asset?.created,
+          )}`}</Text>
+          <Text variant="body2">{`Last updated: ${formatDate(
+            asset?.updated,
+          )}`}</Text>
+          <Text variant="body2">{`Dimensions: ${
+            asset?.dimensions?.width
+          } Χ ${asset?.dimensions?.height} pixels `}</Text>
+          <Text variant="body2">{`Size on disk: ${
+            asset?.size_bytes
+          } bytes`}</Text>
+        </div>
+      </div>
     </StandardDialog>
   );
 }
